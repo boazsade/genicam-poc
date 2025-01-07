52b2af7e603ce5053538cea9f79215fc88e8ee13
Boaz Sade <boaz1sade@gmail.com>
2025-01-07 12:45:43 +0200

    test will show images with opencv
    This will now show the capture image in the test that run with software trigger

--- tests/software_trigger_test/main.cpp
+++ tests/software_trigger_test/main.cpp
@@ -1,11 +1,44 @@
 #include "camera_controller/camera.hh"
 #include "camera_controller/cameras_context.hh"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <thread>
 #include <chrono>
 #include <iostream>
 #include <iterator>
 
 using namespace std::chrono_literals;
+
+inline auto colorize(cv::InputArray input, cv::OutputArray rgb) -> void {
+    cv::cvtColor(input, rgb, cv::COLOR_BayerRG2RGB);
+}
+
+auto show_image(const std::uint8_t* data, int width, int height, int number, const char* name) -> void {
+    using namespace std::string_literals;
+    constexpr auto row_step = 30;       
+    auto row = row_step;
+
+    cv::Mat rgb(height, width, CV_8UC1, (void*)data);
+    colorize(rgb, rgb);
+    cv::resize(rgb, rgb, cv::Size(), 0.5, 0.5);
+    std::string buf("FrameData:  #: "s + std::to_string(number));
+    putText(rgb, buf.c_str(), cv::Point(row_step, row), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 0, 255), 2);
+    row += row_step;
+    auto title{"Image "s + name};
+    cv::imshow(title, rgb);
+    cv::waitKey(1);
+}
+
+auto show_images(const camera::ImageView& frames) -> void {
+    if (frames.size) {
+        show_image(frames.data, frames.width, frames.height, frames.number, "left");
+    } else {
+        std::cerr << "missing frame cannot display\n";
+    }
+    
+}
 
 auto open_device(const std::vector<camera::DeviceInfo>& devices, const camera::Context& ctx) -> std::shared_ptr<camera::IdleCamera> {
     for (auto&& di : devices) {
@@ -20,7 +53,7 @@ auto open_device(const std::vector<camera::DeviceInfo>& devices, const camera::C
 }
 
 auto capture_images_processing(camera::ImageView image) -> bool {
-    std::cout << "successfully got new image " << image << " from the device as the result of the software trigger" << std::endl;
+    show_images(image);
     return true;        // dont do stop from this context
 }
 
@@ -71,6 +104,7 @@ auto main(int argc, char** argv) -> int {
     auto cc{camera::From(std::move(camera))};
     for (auto i = 0; i < 20 && camera::async_software_capture_one(*software_ctx, *cc); i++) {
         std::cout << "Triggered for the " << i << " time successfully " << std::endl;
+        std::this_thread::sleep_for(500ms);
     }
     std::cout << "finish doing the software trigger test" << std::endl;
     
